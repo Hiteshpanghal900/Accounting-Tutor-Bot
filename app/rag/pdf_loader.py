23895b059918f8d3da05435109e6163548d44ac7
hiteshpanghal900 <hiteshpanghal900@gmail.com>
2025-12-29 18:17:13 +0530

    Created pdf loader for reading content

---
+++ app/rag/pdf_loader.py
@@ -0,0 +1,17 @@
+from pypdf import PdfReader
+from pathlib import Path
+from pypdf import PdfReader
+
+def load_pdf_text(file_path: str) -> str:
+    reader = PdfReader(file_path)
+    text = ""
+
+    print(f"Total pages: {len(reader.pages)}")
+
+    for i, page in enumerate(reader.pages):
+        page_text = page.extract_text()
+
+        if page_text:
+            text += page_text + "\n"
+
+    return text
